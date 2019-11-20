ee049a1884a71b7a864603ed9ec92142b4f140ae
ngj0001 <54417269+ngj0001@users.noreply.github.com>
2019-11-19 23:10:26 -0600

    Add files via upload

---
+++ src/main/java/ManagePurchaseUI.java
@@ -0,0 +1,403 @@
+import com.google.gson.Gson;
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class ManagePurchaseUI {
+    public JFrame view;
+
+    public JButton btnLoad = new JButton("Load");
+    public JButton btnAdd = new JButton("Save");
+
+    public JTextField txtPurchaseID = new JTextField(10);
+    public JTextField txtCustomerID = new JTextField(10);
+    public JTextField txtProductID = new JTextField(10);
+    public JTextField txtQuantity = new JTextField(10);
+
+    public JLabel labPrice = new JLabel("Product Price: ");
+    public JLabel labDate = new JLabel("Date of Purchase: ");
+
+    public JLabel labCustomerName = new JLabel("Customer Name: ");
+    public JLabel labProductName = new JLabel("Product Name: ");
+
+    public JLabel labCost = new JLabel("Cost: $0.00 ");
+    public JLabel labTax = new JLabel("Tax: $0.00");
+    public JLabel labTotalCost = new JLabel("Total Cost: $0.00");
+
+    ProductModel product;
+    PurchaseModel purchase;
+    CustomerModel customer;
+
+    public ManagePurchaseUI() {
+        this.view = new JFrame();
+
+        view.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        view.setTitle("Add Purchase");
+        view.setSize(600, 400);
+        view.getContentPane().setLayout(new BoxLayout(view.getContentPane(), BoxLayout.PAGE_AXIS));
+
+        JPanel line = new JPanel(new FlowLayout());
+        line.add(new JLabel("PurchaseID "));
+        line.add(txtPurchaseID);
+        line.add(labDate);
+        view.getContentPane().add(line);
+
+        line = new JPanel(new FlowLayout());
+        line.add(new JLabel("CustomerID "));
+        line.add(txtCustomerID);
+        line.add(labCustomerName);
+        view.getContentPane().add(line);
+
+        line = new JPanel(new FlowLayout());
+        line.add(new JLabel("ProductID "));
+        line.add(txtProductID);
+        line.add(labProductName);
+        view.getContentPane().add(line);
+
+        line = new JPanel(new FlowLayout());
+        line.add(new JLabel("Quantity "));
+        line.add(txtQuantity);
+        line.add(labPrice);
+        view.getContentPane().add(line);
+
+        line = new JPanel(new FlowLayout());
+        line.add(labCost);
+        line.add(labTax);
+        line.add(labTotalCost);
+        view.getContentPane().add(line);
+
+        line = new JPanel(new FlowLayout());
+        line.add(btnAdd);
+        line.add(btnLoad);
+        view.getContentPane().add(line);
+
+        txtProductID.addFocusListener(new ProductIDFocusListener());
+        txtCustomerID.addFocusListener(new CustomerIDFocusListener());
+        txtQuantity.getDocument().addDocumentListener(new QuantityChangeListener());
+
+        btnAdd.addActionListener(new AddButtonListener());
+        btnLoad.addActionListener(new LoadButtonListener());
+    }
+
+    public void run() {
+        purchase = new PurchaseModel();
+        purchase.mDate = Calendar.getInstance().getTime().toString();
+        labDate.setText("Date of purchase: " + purchase.mDate);
+        view.setVisible(true);
+    }
+
+    private class ProductIDFocusListener implements FocusListener {
+        @Override
+        public void focusGained(FocusEvent focusEvent) {
+
+        }
+
+        @Override
+        public void focusLost(FocusEvent focusEvent) {
+            process();
+        }
+
+        private void process() {
+            String s = txtProductID.getText();
+
+            if (s.length() == 0) {
+                labProductName.setText("Product Name: [not specified!]");
+                return;
+            }
+
+            System.out.println("ProductID = " + s);
+
+            try {
+                purchase.mProductID = Integer.parseInt(s);
+
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null,
+                        "Error: Invalid ProductID", "Error Message",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+            product = StoreManager.getInstance().getDataAdapter().loadProduct(purchase.mProductID);
+
+            if (product == null) {
+                JOptionPane.showMessageDialog(null,
+                        "Error: No product with id = " + purchase.mProductID + " in store!", "Error Message",
+                        JOptionPane.ERROR_MESSAGE);
+                labProductName.setText("Product Name: ");
+
+                return;
+            }
+
+            labProductName.setText("Product Name: " + product.mName);
+            purchase.mPrice = product.mPrice;
+            labPrice.setText("Product Price: " + product.mPrice);
+
+        }
+
+    }
+
+    private class CustomerIDFocusListener implements FocusListener {
+        @Override
+        public void focusGained(FocusEvent focusEvent) {
+
+        }
+
+        @Override
+        public void focusLost(FocusEvent focusEvent) {
+            process();
+        }
+
+        private void process() {
+            String s = txtCustomerID.getText();
+
+            if (s.length() == 0) {
+                labCustomerName.setText("Customer Name: [not specified!]");
+                return;
+            }
+
+            System.out.println("CustomerID = " + s);
+
+            try {
+                purchase.mCustomerID = Integer.parseInt(s);
+
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null,
+                        "Error: Invalid CustomerID", "Error Message",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+            customer = StoreManager.getInstance().getDataAdapter().loadCustomer(purchase.mCustomerID);
+
+            if (customer == null) {
+                JOptionPane.showMessageDialog(null,
+                        "Error: No customer with id = " + purchase.mCustomerID + " in store!", "Error Message",
+                        JOptionPane.ERROR_MESSAGE);
+                labCustomerName.setText("Customer Name: ");
+
+                return;
+            }
+
+            labCustomerName.setText("Customer Name: " + customer.mName);
+
+        }
+
+    }
+
+    private class QuantityChangeListener implements DocumentListener {
+        public void changedUpdate(DocumentEvent e) {
+            process();
+        }
+
+        public void removeUpdate(DocumentEvent e) {
+            process();
+        }
+
+        public void insertUpdate(DocumentEvent e) {
+            process();
+        }
+
+        private void process() {
+            String s = txtQuantity.getText();
+
+            if (s.length() == 0) {
+                //labCustomerName.setText("Customer Name: [not specified!]");
+                return;
+            }
+
+            System.out.println("Quantity = " + s);
+
+            try {
+                purchase.mQuantity = Double.parseDouble(s);
+
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null,
+                        "Error: Please enter an invalid quantity", "Error Message",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+            if (purchase.mQuantity <= 0) {
+                JOptionPane.showMessageDialog(null,
+                        "Error: Please enter an invalid quantity", "Error Message",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+            if (purchase.mQuantity > product.mQuantity) {
+                JOptionPane.showMessageDialog(null,
+                        "Not enough available products!", "Information",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+
+            purchase.mCost = purchase.mQuantity * product.mPrice;
+            purchase.mTax = purchase.mCost * 0.09;
+            purchase.mTotal = purchase.mCost + purchase.mTax;
+
+            labCost.setText("Cost: $" + String.format("%8.2f", purchase.mCost).trim());
+            labTax.setText("Tax: $" + String.format("%8.2f", purchase.mTax).trim());
+            labTotalCost.setText("Total: $" + String.format("%8.2f", purchase.mTotal).trim());
+
+        }
+    }
+
+
+    class LoadButtonListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            PurchaseModel purchase = new PurchaseModel();
+            Gson gson = new Gson();
+            String id = txtPurchaseID.getText();
+
+            if (id.length() == 0) {
+                JOptionPane.showMessageDialog(null, "PurchaseID cannot be null!");
+                return;
+            }
+
+            try {
+                purchase.mPurchaseID = Integer.parseInt(id);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Purchase is invalid!");
+                return;
+            }
+
+            // do client/server
+
+            try {
+                Socket link = new Socket("localhost", 2000);
+                Scanner input = new Scanner(link.getInputStream());
+                PrintWriter output = new PrintWriter(link.getOutputStream(), true);
+
+                MessageModel msg = new MessageModel();
+                msg.code = MessageModel.GET_PURCHASE;
+                msg.data = Integer.toString(purchase.mPurchaseID);
+                output.println(gson.toJson(msg)); // send to Server
+
+                msg = gson.fromJson(input.nextLine(), MessageModel.class);
+
+                if (msg.code == MessageModel.OPERATION_FAILED) {
+                    JOptionPane.showMessageDialog(null, "Purchase does NOT exists!");
+                }  else {
+                    purchase = gson.fromJson(msg.data, PurchaseModel.class);
+
+                    product = StoreManager.getInstance().getDataAdapter().loadProduct(purchase.mProductID);
+                    customer = StoreManager.getInstance().getDataAdapter().loadCustomer(purchase.mCustomerID);
+
+                    labCustomerName.setText("Customer Name: " + customer.mName);
+                    labProductName.setText("Product Name: " + product.mName);
+                    labPrice.setText("Product Price: $" + String.format("%8.2f", product.mPrice).trim());
+
+                    labCost.setText("Cost: $" + String.format("%8.2f", purchase.mCost).trim());
+                    labTax.setText("Tax: $" + String.format("%8.2f", purchase.mTax).trim());
+                    labTotalCost.setText("Total: $" + String.format("%8.2f", purchase.mTotal).trim());
+
+                    txtCustomerID.setText(Integer.toString(purchase.mCustomerID));
+                    txtProductID.setText(Integer.toString(purchase.mProductID));
+                    txtQuantity.setText(Double.toString(purchase.mQuantity));
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
+    class AddButtonListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            Gson gson = new Gson();
+            String purchaseID = txtPurchaseID.getText();
+
+            if (purchaseID.length() == 0) {
+                JOptionPane.showMessageDialog(null, "PurchaseID cannot be null!");
+                return;
+            }
+
+            try {
+                purchase.mPurchaseID = Integer.parseInt(purchaseID);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "PurchaseID is invalid!");
+                return;
+            }
+
+            String productID = txtProductID.getText();
+
+            if (productID.length() == 0) {
+                JOptionPane.showMessageDialog(null, "ProductID cannot be null!");
+                return;
+            }
+
+            try {
+                purchase.mProductID = Integer.parseInt(productID);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "ProductID is invalid!");
+                return;
+            }
+
+            //
+            String customerID = txtCustomerID.getText();
+
+            if (customerID.length() == 0) {
+                JOptionPane.showMessageDialog(null, "CustomerID cannot be null!");
+                return;
+            }
+
+            try {
+                purchase.mCustomerID = Integer.parseInt(customerID);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "CustomerID is invalid!");
+                return;
+            }
+
+            String quant = txtQuantity.getText();
+            try {
+                purchase.mQuantity = Double.parseDouble(quant);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Quantity is invalid!");
+                return;
+            }
+
+            // all purchase info is ready! Send to Server!
+
+            try {
+                Socket link = new Socket("localhost", 2000);
+                Scanner input = new Scanner(link.getInputStream());
+                PrintWriter output = new PrintWriter(link.getOutputStream(), true);
+
+                MessageModel msg = new MessageModel();
+                msg.code = MessageModel.PUT_PURCHASE;
+                msg.data = gson.toJson(purchase);
+                output.println(gson.toJson(msg)); // send to Server
+
+                msg = gson.fromJson(input.nextLine(), MessageModel.class); // receive from Server
+
+                if (msg.code == MessageModel.OPERATION_FAILED) {
+                    JOptionPane.showMessageDialog(null, "Purchase is NOT saved successfully!");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Purchase is SAVED successfully!");
+                    view.dispose();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+//
+
+}
